469c30c537379c8e5b3b568449bf0fb288d0d594
Your Name <you@example.com>
2021-12-02 16:39:45 +0100

    add hangman

---
+++ Hangman/Hangman/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+  internal class Word
+  {
+    private string[] _wordlist;
+    public Word()
+    {
+      _wordlist = ReadFile("wordlist.txt");
+    }
+
+    public string WordList { get; }
+
+    private string[] ReadFile(string filename)
+    {
+      try
+      {
+        using var sr = new StreamReader(filename);
+        return sr.ReadToEnd().Split("\n");
+      }
+      catch (IOException e)
+      {
+        Console.WriteLine("The file could not be read:");
+        Console.WriteLine(e.Message);
+        return null;
+      }
+    }
+    
+  }
+}
---
+++ Hangman/Hangman/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hangman
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private static readonly char[] wordArray = { 'a', 'm', 'a', 'g' };
+
+    private bool IsPartOfWord(char inputChar, char[] wordArray)
+    {
+      return wordArray.Contains(inputChar);
+    }
+
+    private char GetInputChar()
+    {
+      char selectedChar = Convert.ToChar((int)Letter_Slider.Value);
+      return selectedChar;
+    }
+
+    private void Guess_Btn_Click(object sender, RoutedEventArgs e)
+    {
+      char guessedChar = GetInputChar();
+
+      switch (IsPartOfWord(guessedChar, wordArray))
+      {
+        case true:
+          MessageBox.Show("Korrekt, der Buchstabe " + guessedChar + " ist im Wort enthalten.");
+          break;
+        case false:
+          MessageBox.Show(guessedChar + " ist nicht Teil des Wortes.");
+          break;
+      }
+    }
+
+    private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+    {
+      char inputChar = GetInputChar();
+      Selected_Char.Text = inputChar.ToString();
+    }
+  }
+}
